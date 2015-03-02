e3b8b97a610db9f2056de8d010614f84d2256be5
soye <sophixer@gmail.com>
2015-03-01 22:33:32 -0800

    fixed bug, map is now full browser window, infowindows automatically close upon opening of another object

--- js/custom.js
+++ js/custom.js
@@ -1,32 +1,31 @@
-var map;
+var map = null;
+var openInfoWindow = null;
+var currentLocation = null;
+
 
 function initialize() {
 	var mapProp = {
 		center:new google.maps.LatLng(39.828127,-98.579404), 
-    zoom:5, 
-    mapTypeId:google.maps.MapTypeId.ROADMAP
+    	zoom: 5, 
+    	mapTypeId:google.maps.MapTypeId.ROADMAP
 	};
 
 	map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
 	google.maps.event.addListener(map, 'click', function(event) {
+		if (openInfoWindow) 
+			openInfoWindow.close();
 		placeMarker(event.latLng);
 	});
 
 }
 
-var currentMarker;
-
 function placeMarker(location) {
-	var marker = new google.maps.Marker({
-		position: location,
-		map: map
-	});
-	currentMarker = marker;
-
 	// opens the form
 	$('#form').trigger("reset");
 	$('#modal-form').foundation('reveal', 'open');
+
+	currentLocation = location;
 
 	overlay();  		
 }
@@ -34,16 +33,23 @@ function placeMarker(location) {
 function postEntry() {
 	$('#modal-form').foundation('reveal', 'close');
 
-  infowindow = new google.maps.InfoWindow({
-    content: '<h4>' + $("#event-name").val() + '</h4>' +
-    		'<p><b>Year:</b> ' + $("#year").val() + '</p>' +
-    		'<p>' + $("#details").val() + '</p>'
-  });
+	var infowindow = new google.maps.InfoWindow({
+	content: '<h4>' + $("#event-name").val() + '</h4>' +
+			'<p><b>Year:</b> ' + $("#year").val() + '</p>' +
+			'<p>' + $("#details").val() + '</p>'
+	});
 
-  thisMarker = currentMarker;
-  google.maps.event.addListener(thisMarker, 'click', function(e) {
-	infowindow.open(map, thisMarker);
-  });
+	var marker = new google.maps.Marker({
+		position: currentLocation,
+		map: map
+	});
+
+	google.maps.event.addListener(marker, 'click', function(e) {
+		if (openInfoWindow)
+			openInfoWindow.close();
+		infowindow.open(map, marker);
+		openInfoWindow = infowindow;
+	});
 }
 
 
